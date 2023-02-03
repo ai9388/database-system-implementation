7532b59d86b7b50f865903fe0899178795f5a903
aas3992 <aas3992@rit.edu>
2023-02-03 13:48:26 -0500

    parsing...

--- Main/src/Parser.java
+++ Main/src/Parser.java
@@ -1,3 +1,29 @@
 public class Parser {
-    
+    private String[] args;
+    public Parser(String[] args) {
+        this.args = args;
+    }
+    /**
+     * Assume user passes in database
+     */
+    public void parse() {
+        for (String input : args) {
+            input = input.toLowerCase();
+            if (input.startsWith("create")){
+
+            } else if (input.startsWith("select")) {
+                //
+            } else if (input.startsWith("insert")) {
+                //
+            } else if (input.startsWith("display")){
+                //
+            } else {
+                return;
+            }
+            // create table <name>( <attr1> <attr1type primarykey,
+            //  <attr2> <attr2type,
+            // );
+            //
+        }
+    }
 }
