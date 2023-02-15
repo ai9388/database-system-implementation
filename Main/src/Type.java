1b644ed86f862ffb56bf6f136c401e6cbe3a40e0
Carly Maggiolo <carly.maggiolo@gmail.com>
2023-02-15 11:59:23 -0800

    added methods for data validation and identification

--- Main/src/Type.java
+++ Main/src/Type.java
@@ -14,4 +14,84 @@ public enum Type {
     public String getName() {
         return name;
     }
+
+    /*
+     * validate that the provided value is of type Type
+     * @param value values to check
+     * @param attribute attribute value is expected to belong to
+     */
+    public static boolean validateType(String value, Attribute attribute) throws InvalidDataTypeException{
+        switch(attribute.getType()){
+            case INTEGER:
+                try {
+                    Integer.parseInt(value);
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    // throw custom exception with message
+                    throw new InvalidDataTypeException(value, attribute.getType());
+                }
+            case DOUBLE:
+                try {
+                    Double.parseDouble(value);
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    // throw custom exception with message
+                    throw new InvalidDataTypeException(value, attribute.getType());
+                }
+            case BOOLEAN:
+                try {
+                    Boolean.parseBoolean(value);
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    // throw custom exception with message
+                    throw new InvalidDataTypeException(value, attribute.getType());
+                }
+            case CHAR:
+                try {
+                    if(value.length() > attribute.getN()){
+                        // throw custom exception
+                        return false;
+                    }
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    // throw custom exception with message
+                    throw new InvalidDataTypeException(value, attribute.getType());
+                }
+            case VARCHAR:
+                try {
+                    if(value.length() > attribute.getN()){
+                        // throw custom exception
+                        throw new InvalidDataTypeException(value, attribute.getType());
+                    }
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    // throw custom exception with message
+                    throw new InvalidDataTypeException(value, attribute.getType());
+                }
+        }
+        return true;
+    }
+
+    /*
+     * Identify the type of the provided value
+     * @param value values to identify
+     */
+    public static Type identifyType(String value){
+        try {
+            Integer.parseInt(value);
+            return Type.INTEGER;
+        }
+        catch(Exception e){
+
+        }
+        try {
+            Boolean.parseBoolean(value);
+            return Type.BOOLEAN;
+        }
+        catch(Exception e){
+
+        }
+        return Type.CHAR;
+    }
+
 }
