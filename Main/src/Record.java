be3c035e4fcb78b2441dcc7bcd28e0b2c4d032e2
Carly Maggiolo <carly.maggiolo@gmail.com>
2023-02-15 11:53:08 -0800

    minor changes to facilitate data validation/manipulation

--- Main/src/Record.java
+++ Main/src/Record.java
@@ -6,11 +6,29 @@ public class Record {
      */
     private ArrayList<String> data;
 
-    public Record(String[] values)
+    /*
+     * data by columnName
+     */
+    private HashMap<String, String> dataByColumn;
+
+    public Record(String[] values, ArrayList<Attribute> attributes) throws InvalidDataTypeException
     {
         data = new ArrayList<String>(Arrays.asList(values));
+        validateDataTypeS(attributes);
+
+        // insert all values into map
+        for (int i = 0; i < values.length; i++) {
+            dataByColumn.put(attributes.get(i).getName(), values[i]);
+        }
     }
 
+    /*
+     * returns the value of this record at a specific column
+     * @param column the name of the column
+     */
+    public String getvalueAtColumn(String column){
+        return dataByColumn.get(column);
+    }
     /**
      * @param data the data to set
      */
@@ -21,53 +39,26 @@ public class Record {
     /*
      * compare the value types with the ones from the schema
      */
-    public void validateDataTypeS(ArrayList<Attribute> attributes) throws Exception{
+    public void validateDataTypeS(ArrayList<Attribute> attributes) throws InvalidDataTypeException{
+        try{
         for (int i = 0; i < data.size(); i++) {
             Attribute attribute = attributes.get(i);
             String value = data.get(i);
 
-            switch(attribute.getType()){
-                case INTEGER:
-                    try {
-                        Integer.parseInt(value);
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                        // throw custom exception with message
-                    }
-                case DOUBLE:
-                    try {
-                        Double.parseDouble(value);
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                        // throw custom exception with message
-                    }
-                case BOOLEAN:
-                    try {
-                        Boolean.parseBoolean(value);
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                        // throw custom exception with message
-                    }
-                case CHAR:
-                    try {
-                        if(value.length() > attribute.getN()){
-                            // throw custom exception
-                        }
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                        // throw custom exception with message
-                    }
-                case VARCHAR:
-                    try {
-                        if(value.length() > attribute.getN()){
-                            // throw custom exception
-                        }
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                        // throw custom exception with message
-                    }
-            }
+            // if this validation fails an exception is raised
+            Type.validateType(value, attribute);
         }
+        }
+        catch(InvalidDataTypeException idte){
+            // this means at least one of the attribute entries 
+            // is incorrect
+            throw new InvalidDataTypeException(data, attributes);
+        }
+
+    }
+
+    public ArrayList<String> getData() {
+        return data;
     }
 
 }
