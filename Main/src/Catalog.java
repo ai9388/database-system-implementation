eb914ba0f6356dbc64284cc8afe6dfa7a92b9232
Alex Iacob <ai9388@rit.edu>
2023-02-15 12:27:29 -0500

    bytes can be written to a file

--- Main/src/Catalog.java
+++ Main/src/Catalog.java
@@ -1,10 +1,14 @@
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.file.*;
 import java.util.*;
 
 public class Catalog {
+
+    public final String READ = "r";
+    public final String WRITE = "rw";
     
     private String path;
     private ArrayList<Attribute> attributes;
@@ -19,10 +23,49 @@ public class Catalog {
         this.bufferSize = bufferSize;
     }
 
+    public byte[] convertIntToByteArray(int v) 
+    {
+        return ByteBuffer.allocate(4).putInt(v).array();
+    }
+
+    public int convertByteArrayToInt(byte[] bytes) 
+    {
+        return ByteBuffer.wrap(bytes).getInt();
+    }
+
+    public byte convertBooleanToByte(boolean bool) 
+    {
+        return (byte) (bool ? 1 : 0);
+    }
+
+    public boolean convertByteToBoolean(byte b) 
+    {
+        return b != 0;
+    }
+
+    public byte[] convertDoubleToByteArray(double d) 
+    {
+        return ByteBuffer.allocate(8).putDouble(d).array();
+    }
+
+    public byte[] convertCharToByteArray(char c) 
+    {
+        return ByteBuffer.allocate(8).putChar(c).array();
+    }
+
+    public byte[] convertStringToByteArray(String st)
+    {
+        byte[] bb = new byte[st.length()];
+        
+        for (int i = 0; i < bb.length; i++) {
+            bb[i] = (byte)st.charAt(i);
+        }
+
+        return bb;
+    }
+
     public void writeToFile()
     {
-
-        File file = new File(this.path + "Catalog.txt");
         String content = "";
 
         content += "DB Location: " + this.path + "\n";
@@ -30,17 +73,15 @@ public class Catalog {
         content += "Buffer size: " + this.bufferSize + "\n";
 
         try {
-            FileWriter myWriter = new FileWriter(file.getPath());
-            myWriter.write(content);
-            myWriter.close();
+            File file = new File(this.path + "Catalog");
+            RandomAccessFile raf = new RandomAccessFile(file, WRITE);
+
+            raf.write(convertStringToByteArray(content));
+
+            raf.close();
         } catch (IOException e) {
             System.out.println("oh no...");
-            System.out.println(file.getAbsolutePath().toString());
             e.printStackTrace();
         }
-        
-        //byte[] bytes = new byte[size];
-
     }
-        
 }
