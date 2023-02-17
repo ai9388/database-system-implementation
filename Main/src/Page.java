de2adfc0c716ad2605b783391d3232a32e9372a8
hai-yennguyen <hnn2306@rit.edu>
2023-02-17 13:26:27 -0500

    create page class and working on the storage

---
+++ Main/src/Page.java
@@ -0,0 +1,13 @@
+public class Page {
+    
+    /***
+     * So the page contains the list of records - need to discuss 
+     * Page have fix size, use static
+     * page have the length to see how many records in it
+     * store the pages in the table with a datasturture to get the page number
+     * Have a split method to split the page to 2 pages. 
+     */
+
+     
+    
+}
