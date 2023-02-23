91f6077432efff851dc2e2c9dc9fe9b8d72a5978
newcarlis <60241143+newcarlis@users.noreply.github.com>
2023-02-22 19:59:15 -0500

    minor changes to output format

--- Main/src/InvalidDataTypeException.java
+++ Main/src/InvalidDataTypeException.java
@@ -13,11 +13,11 @@ public class InvalidDataTypeException extends Exception{
         super();
         if(attribute.getType() == Type.CHAR || attribute.getType() == Type.VARCHAR){
             this.expected = (attribute.getType().getName() + "(" + attribute.getN() + ")");
-            this.got = (Type.identifyType(value) + " (" + value.length() + ")");
+            this.got = (Type.identifyType(value, attribute).getName() + " (" + value.length() + ")");
         }
         else{
             this.expected = attribute.getType().getName();
-            got += Type.identifyType(value);
+            got += Type.identifyType(value, attribute).getName();
         }
     }
 
@@ -31,20 +31,20 @@ public class InvalidDataTypeException extends Exception{
     /*
      * Used when creating a record
      */
-    public InvalidDataTypeException(ArrayList<String> values, ArrayList<Attribute> attributes){
+    public InvalidDataTypeException(String[] values, ArrayList<Attribute> attributes){
         super();
         expected = "";
         got = "";
-        for (int i = 0; i < values.size(); i++) {
+        for (int i = 0; i < values.length; i++) {
             Attribute a = attributes.get(i);
-            String v = values.get(i);
+            String v = values[i];
             if(a.getType() == Type.CHAR || a.getType() == Type.VARCHAR){
                 this.expected += (a.getType().getName() + "(" + a.getN() + ")");
-                this.got += (Type.identifyType(v) + "(" + v.length() + ")");
+                this.got += (Type.identifyType(v, a).getName() + "(" + v.length() + ")");
             }
             else{
                 this.expected += a.getType().getName() ;
-                this.got += Type.identifyType(v);
+                this.got += Type.identifyType(v, a).getName();
             }
 
             expected += " ";
