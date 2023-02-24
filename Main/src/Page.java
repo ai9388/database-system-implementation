6de5a451dc4d1d9c2ffc504e06f9fc0314b878e7
newcarlis <60241143+newcarlis@users.noreply.github.com>
2023-02-24 15:22:24 -0500

    added split implementation and other page methods

--- Main/src/Page.java
+++ Main/src/Page.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Page {
     
     /***
      * So the page contains the list of records - need to discuss 
      * Page have fix size, use static
      * page have the length to see how many records in it
-     * store the pages in the table with a datasturture to get the page number
+     * store the pages in the table with a data structure to get the page number
      * Have a split method to split the page to 2 pages. 
      * 
      * Methods needed:
@@ -30,30 +35,63 @@ public class Page {
     private int size;
 
     /**
-     * string content, typically records in string form
-     */
-    private String content;
-
-    /**
-     * default size of a byte in java
-     */
-    private static final int BYTESIZE = 2;
-
-    /**
      * the id of this page
      */
     private int id;
 
     StorageManager sm;
 
-    /**
-     * used to convert an existing page from memory into records
-     * @param content
-     */
-    public Page(String content){
-        this.content = content;
-        this.size = content.length() * BYTESIZE;
+    ArrayList<Record> records;
+
+    int pkIdx;
+    
+    public Page(int pkIdx){
+        this.size = 0;
         sm = new StorageManager();
+        this.pkIdx = pkIdx;
+    }
+
+    public void setRecordsOrder() {
+        Collections.sort(records, new Comparator<Record>() {
+            @Override
+            public int compare(Record r1, Record r2){
+                Object pkValue1 = r1.getValueAtColumn(pkIdx);
+                Object pkValue2 = r2.getValueAtColumn(pkIdx);
+
+                // INT
+                if(pkValue1 instanceof Integer){
+                    return Integer.compare((int)pkValue1, (int)pkValue2);
+                }
+
+                // DOUBLE
+                else if(pkValue1 instanceof Double){
+                    return Double.compare((double)pkValue1, (double)pkValue2);
+                }
+
+                // BOOLEAN
+                else if(pkValue1 instanceof Boolean){
+                    return Boolean.compare((boolean)pkValue1, (boolean)pkValue2);
+                }
+
+                // String
+                else{
+                    return String.valueOf(pkValue1).compareTo(String.valueOf(pkValue2));
+                }
+                
+            }
+        });
+    }
+
+    public Page(ArrayList<Record> records){
+        this.records = records;
+        sm = new StorageManager();
+        setSize();
+    }
+
+    public void setSize() {
+        for(Record r: records){
+            this.size += r.recordToBytes().length;
+        }
     }
 
     /**
@@ -72,6 +110,16 @@ public class Page {
         return capacity - size;
     }
 
+    public boolean addRecord(Record record){
+        if(fit(record)){
+            this.records.add(record);
+            // increment the size
+            this.size += record.recordToBytes().length;
+            return true;
+        }
+        return false;
+    }
+
     /**
      * return the current capacity
      * @return int
@@ -86,6 +134,22 @@ public class Page {
 
     public byte[] getHeader(){
         return sm.concat(sm.convertIntToByteArray(this.id), sm.convertIntToByteArray(this.size));
+    }
+
+    public Page split(Record record){
+        // add the record
+        this.records.add(record);
+        // sort the list with the new record
+        setRecordsOrder();
+
+        // SPLIT
+        // get the middle index
+        int idx = Math.ceilDiv(records.size(), 2);
+        Page otherPage = new Page(new ArrayList<>(this.records.subList(idx + 1, records.size() + 1)));
+        this.records = new ArrayList<>(this.records.subList(0, idx + 1));
+        this.setSize();
+
+        return otherPage;
     }
     
 }
