e001b0b03ae89a48ecf29b21360a8d8efcdf246d
newcarlis <60241143+newcarlis@users.noreply.github.com>
2023-02-25 12:59:19 -0500

    new exception case added

--- Main/src/TableException.java
+++ Main/src/TableException.java
@@ -1,6 +1,7 @@
 public class TableException extends Exception{
     private String[] errorMessages = new String[]{
         "Invalid Column Name: ",
+        "Invalid Table Name: "
     };
     private String message = "Table Error:";
 
