f5a84d95ebde77c6c0a7cc0f45dfc57f23740edd
newcarlis <60241143+newcarlis@users.noreply.github.com>
2023-03-09 00:42:35 -0500

    added table id to facilitate serialization
    @ai9388 after creating a page object in catalog, either call setTableId() or add another parameter to the constructor to pass in the table ID. whatever works best.

--- Main/src/Page.java
+++ Main/src/Page.java
@@ -25,12 +25,13 @@ public class Page {
 
     ArrayList<Record> records;
 
-    int pageNumber;
+    int tableId;
     
-    public Page(int id){
+    public Page(int id, int tableId){
         this.size = 0;
         this.records = new ArrayList<>();
         this.id = id;
+        this.tableId = tableId;
     }
 
     /**
@@ -39,6 +40,7 @@ public class Page {
      * @param records
      */
     public Page(int id, ArrayList<Record> records){
+        this.id = id;
         this.records = records;
         this.size = 0;
         setSize();
@@ -50,6 +52,10 @@ public class Page {
         for(Record r: this.records){
             this.size += r.getSize();
         }
+    }
+
+    public void setTableId(int tableId) {
+        this.tableId = tableId;
     }
 
     /**
@@ -82,10 +88,6 @@ public class Page {
 
     public byte[] getHeader(){
         return Type.concat(Type.convertIntToByteArray(this.id), Type.convertIntToByteArray(this.size));
-    }
-
-    public int getPageNumber() {
-        return pageNumber;
     }
 
     public byte[] recordsAsBytes()
