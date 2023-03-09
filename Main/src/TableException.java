12f78643ef31edc1e9df98f75bcd5ebf28963258
newcarlis <60241143+newcarlis@users.noreply.github.com>
2023-03-09 00:56:45 -0500

    added other error codes

--- Main/src/TableException.java
+++ Main/src/TableException.java
@@ -1,15 +1,17 @@
 public class TableException extends Exception{
     private String[] errorMessages = new String[]{
-        "Invalid Column Name: ",
-        "Invalid Table Name: ",
-        "Too many entries!",
-        "Missing Values! "
+        "Invalid Column Name: %s", // 1
+        "Invalid Table Name: %s", // 2
+        "Too many entries!", // 3
+        "Missing Values! ", // 4
+        "Table %s already exists!", // 5
+        "Column %s already exists!" // 6
     };
     private String message = "Table Error:";
 
     public TableException(int errorCode, String arg){
         super();
-        this.message = errorMessages[errorCode - 1] + arg;
+        this.message = String.format(errorMessages[errorCode - 1], arg);
 
     }
 
